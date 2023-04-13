699b4a252b639169c99e4cb0846c1896e923c3cb
arisssm <arisssmulianto@gmail.com>
2023-04-13 10:56:34 +0700

    fix button delete on keranjang

--- src/components/TableComp.vue
+++ src/components/TableComp.vue
@@ -31,7 +31,7 @@
                                             <i class="fa fa-plus-circle"></i>
                                         </button>
                                     </td>
-                                    <td class="py-5 text-center text-custom">Rp. {{ item.produk.harga }}</td>
+                                    <td class="py-5 text-center text-custom">Rp. {{item.produk.harga}}</td>
                                     <td class="py-5 text-center">
                                         <button type="button" class="btn btn-danger" v-on:click="hapus(item.id)"><i
                                                 class="fa-solid fa-trash" aria-hidden="true"></i></button>
@@ -48,7 +48,7 @@
                     <p style="font-weight:600;">
                         Total Harga:
                         <span class="mx-5">
-                            Rp. 21.000.000
+                            Rp. {{ Number(totalHarga).toLocaleString() }}
                         </span>
                     </p>
                     <router-link to="/checkoutpage" class="btn mt-5 me-5 btn-checkout-keranjang">Checkout <i
@@ -61,6 +61,7 @@
 
 <script>
 import axios from 'axios';
+
 export default {
     name: "TableComp",
     props: ["keranjang"],
@@ -70,30 +71,33 @@ export default {
         }
     },
     methods: {
-        tambah(item) {
+        async tambah(item) {
             item.banyak += 1;
             if (item.banyak > item.produk.stok) {
                 item.banyak = item.produk.stok;
             }
+
+            await axios.put('http://127.0.0.1:8000/api/keranjang/' + item.id, item);
+            //console.log(item.banyak);
         },
-        kurang(item) {
+        async kurang(item) {
             item.banyak -= 1;
             if (item.banyak < 1) {
                 item.banyak = 1;
             }
+            await axios.put('http://127.0.0.1:8000/api/keranjang/' + item.id, item);
+            //console.log(item.banyak);
         },
-        hapus(id) {
-            axios
-                .delete('http://127.0.0.1:8000/api/keranjang/' + id)
-                .then((response) => {
-                    alert(response);
-                    // console.log(response);
-                })
-                .catch((error) => {
-                    alert('Gagal menghapus keranjang' + error);
-                    //console.log(error);
-                })
+        hapus(id){
+            this.$emit('buttonDelete', id);
         },
+    },
+    computed: {
+        totalHarga(){
+            return this.keranjang.reduce(function (item, data ) {
+                return item + data.produk.harga * data.banyak;
+            },0)
+        }
     }
 };
 </script>
--- src/views/KeranjangPage.vue
+++ src/views/KeranjangPage.vue
@@ -4,13 +4,13 @@
         <div class="container my-5">
             <div class="row">
                 <div class="col">
-                    <router-link to="/productdetailpage" style="text-decoration: none;">
+                    <router-link to="/katalogpage" style="text-decoration: none;">
                     <h6 style="color:#000000;"><i style="color:#ff0000;" class="fa fa-arrow-circle-left" aria-hidden="true"></i> Keranjang</h6>
                     </router-link>
                 </div>
             </div>
         </div>
-        <TableComp :keranjang="keranjang"/>
+        <TableComp :keranjang="keranjang" @buttonDelete="hapus($event)"/>
         
     </div>
 </template>
@@ -46,9 +46,28 @@ export default{
                 //console.log(error);
             })
         },
+        hapus(id) {
+            axios
+                .delete('http://127.0.0.1:8000/api/keranjang/' + id)
+                .then((response) => {
+                    this.$toast.success(response.data.message, {
+                            position: "top",
+                            duration: 3000,
+                            type: "success",
+                        });
+                })
+                .catch((error) => {
+                    this.$toast.error(error, {
+                            position: "top",
+                            duration: 3000,
+                            type: "error",
+                        });
+                })
+        },
     },
     mounted() {
         this.getKeranjang();
+        
     },
 };
 </script>
